a0c6cde90d4853775ffee73298cd1d6dac75c743
Daniel Betancourt <daniel.betancourt@duck.com>
2024-11-03 20:52:45 -0700

    Week 2

---
+++ Readme.md
@@ -0,0 +1,1 @@
+# Basic Fluid Layout
---
+++ src/app.js
@@ -0,0 +1,19 @@
+var object = {
+    name: 'Danno',
+    age: 41
+};
+
+class Bob {
+    name;
+    age;
+
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+
+  
+}
+
+
+console.log(new Bob('Danno', 41));
