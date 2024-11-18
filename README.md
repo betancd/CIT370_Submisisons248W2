1ec119220b1573ddd9de3a01e1f0b55cccde2d3b
Daniel Betancourt <danielbetancourt@duck.com>
2024-11-18 02:56:47 -0700

    <Final-Project v1>

---
+++ README.md
@@ -0,0 +1,11 @@
+#Navy Memorial 
+
+This website is intended to help consolodate memories from a 22 year Navy career and provide information to 
+anyone who is interested in what Navy life is like.
+
+When complete, it will feature:
+-Photos
+-Ships and their history
+-Countries/Ports and interesting information about them
+-Significance of various regions (Red Sea, Straights of Malacca, etc)
+-Awarads and their meanings
---
+++ server/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const path = require('path');
+
+const port = process.env.PORT || 4000;
+
+app.use(express.static('public'));
+
+app.use('/css', express.static(__dirname + '/public/css')); //http://localhost:3000/css
+app.use('/js', express.static(__dirname + '/public/src')); //http://localhost:3000/js
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'public', 'home.html'));
+  });
+
+app.listen(port, function() {
+    console.log('Server started at http://localhost:%s', port);
+});
